9d776611eef3c42727d02408613750c7a8f8a623
oluizoliveira997 <oliveiraluiz@unitins.br>
2025-11-11 14:20:00 -0300

    criação de algumas classes (Luiz)

---
+++ API/Migrations/20251120154757_FixStudyActivityRelations.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStudyActivityRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudyActivities_StudyTracks_StudyTrackId1",
+                table: "StudyActivities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudyTracks_Users_UserId",
+                table: "StudyTracks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudyTracks_UserId",
+                table: "StudyTracks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudyActivities_StudyTrackId1",
+                table: "StudyActivities");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "StudyTracks");
+
+            migrationBuilder.DropColumn(
+                name: "StudyTrackId1",
+                table: "StudyActivities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "StudyTracks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudyTrackId1",
+                table: "StudyActivities",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyTracks_UserId",
+                table: "StudyTracks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyActivities_StudyTrackId1",
+                table: "StudyActivities",
+                column: "StudyTrackId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudyActivities_StudyTracks_StudyTrackId1",
+                table: "StudyActivities",
+                column: "StudyTrackId1",
+                principalTable: "StudyTracks",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudyTracks_Users_UserId",
+                table: "StudyTracks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ API/Models/StudyActivity.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace Api.Models
+{
+    public enum ActivityStatus
+    {
+        Pendente,
+        EmAndamento,
+        Concluida
+    }
+
+    public class StudyActivity
+    {
+        public int Id { get; set; }
+        public int StudyTrackId { get; set; }
+        
+        [JsonIgnore]
+        public StudyTrack? StudyTrack { get; set; }
+        
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Link { get; set; }
+        public int Order { get; set; }
+        public ActivityStatus ActivityStatus { get; set; }
+    }
+}
