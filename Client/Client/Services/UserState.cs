11f98914c87f78d2c33ebb3d5a0b2257a9d8d2ff
oluizoliveira997 <oliveiraluiz@unitins.br>
2025-11-27 00:42:01 -0300

    Commit do Client (Luiz)

---
+++ Client/Client/Services/UserState.cs
@@ -0,0 +1,12 @@
+namespace EduMentorClient.Services
+{
+    public class UserState
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+        public bool IsLoggedIn => !string.IsNullOrEmpty(Token);
+    }
+}
